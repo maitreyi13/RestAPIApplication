7b81372ce1de208fa8a272451f2f2f45884ffe2f
maitreyi13 <kulkarnimaitreyi0@gmail.com>
2022-09-22 10:43:57 +0530

    Added: Calling using Name

--- src/main/java/com/springbootrestapi/controller/RestAPI.java
+++ src/main/java/com/springbootrestapi/controller/RestAPI.java
@@ -1,12 +1,14 @@
 package com.springbootrestapi.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class RestAPI {
-    @RequestMapping("/api")
-    public String Hello (){
+    @RequestMapping(value = {"","/","/home"}, method = RequestMethod.GET)
+    public String welcome(){
         return "<h1>Hello From BridgeLabz!!</h1>";
+    }
+    @RequestMapping(value = {"/greet/{name}"}, method = RequestMethod.GET)
+    public String greet(@PathVariable String name) {
+        return "Hello, " + name + " from BridgeLabz!!";
     }
 }
